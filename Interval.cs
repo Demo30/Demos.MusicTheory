e60217311ef14d49a559ec396e0c2b2eccc1eae7
Tomáš Wiesner <tomas.wiesner@ruk.cuni.cz>
2020-11-29 18:52:32 +0100

    wip

---
+++ Interval.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.MusicTheory
+{
+    public class Interval
+    {
+        public int Number { get; }
+
+        public Interval(int number)
+        {
+            if (number > 0)
+            {
+                this.Number = number;
+            } else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ IntervalQuality.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.MusicTheory
+{
+    public enum IntervalQuality
+    {
+        Perfect,
+        Minor,
+        Major,
+        Augmented,
+        Diminished
+    }
+}
