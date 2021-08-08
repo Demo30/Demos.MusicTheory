c2d9a9dc8acfa05a42802b5295d5326ed2e4f61d
Demo30 <wiesnertoma@gmail.com>
2021-08-08 20:33:31 +0200

    Unit test project added

--- ChromaticNote.cs
+++ ChromaticNote.cs
@@ -9,30 +9,16 @@ namespace Demos.MusicTheory
     public class ChromaticNote
     {
         public ElementaryChromaticNotes ElementaryNote { get; }
-        public int OctaveOrder { get; set; } = 0;
+        public int OctaveOrder { get; } = 0;
         public NotationSymbols NoteModifierSymbol { get; } = NotationSymbols.None;
-        public int StaffPositionIndex { get; set; }
+        
+        public int StaffPositionIndex { get; } // This is not really a chromatic note property
 
         //public Tone Tone { get; } // freq?
 
         public ChromaticNote(ElementaryChromaticNotes elementaryNote, int octaveOrder, NotationSymbols modifier, int staffPosition)
         {
-            if (octaveOrder < 0)
-            {
-                throw new ArgumentException("Octave order cannot be negative.");
-            }
-
-            if (staffPosition < 0)
-            {
-                throw new ArgumentException("Staff position index cannot be negative.");
-            }
-
-            NotationSymbols[] acceptedSymbols = new NotationSymbols[] { NotationSymbols.Sharp, NotationSymbols.Flat, NotationSymbols.None };
-
-            if (!acceptedSymbols.Contains(modifier))
-            {
-                throw new ArgumentException("Notation symbol is none of accepted types.");
-            }
+            CheckConstructionArgumentValidity(elementaryNote, octaveOrder, modifier, staffPosition);
 
             this.ElementaryNote = elementaryNote;
             this.OctaveOrder = octaveOrder;
@@ -50,8 +36,29 @@ namespace Demos.MusicTheory
 
         public override string ToString()
         {
-            string modifier = this.NoteModifierSymbol == NotationSymbols.None ? "" : this.NoteModifierSymbol.ToString();
-            return $"{this.ElementaryNote}{this.OctaveOrder}{modifier}";
+            string modifier = NoteModifierSymbol == NotationSymbols.None ? "" : NoteModifierSymbol.ToString();
+            return $"{ElementaryNote}{OctaveOrder}{modifier}";
+        }
+
+        private NotationSymbols[] GetRelevantNotationSymbols() => new NotationSymbols[] { NotationSymbols.Sharp, NotationSymbols.Flat, NotationSymbols.None };
+
+        private void CheckConstructionArgumentValidity(ElementaryChromaticNotes elementaryNote, int octaveOrder, NotationSymbols modifier, int staffPosition)
+        {
+            if (octaveOrder < 0)
+            {
+                throw new ArgumentException("Octave order cannot be negative.");
+            }
+
+            if (staffPosition < 0)
+            {
+                throw new ArgumentException("Staff position index cannot be negative.");
+            }
+
+            NotationSymbols[] acceptedSymbols = GetRelevantNotationSymbols();
+            if (!acceptedSymbols.Contains(modifier))
+            {
+                throw new ArgumentException("Notation symbol is none of accepted types.");
+            }
         }
     }
 }
---
+++ Demos.MusicTheory.Tests/Tests/NoteCreationTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Demos.MusicTheory.Tests
+{
+    [TestFixture]
+    public class NoteCreationTests
+    {
+        [Theory]
+        [TestCase(ElementaryChromaticNotes.E, 1, NotationSymbols.None, 1)]
+        [TestCase(ElementaryChromaticNotes.D, 5, NotationSymbols.Sharp, 20)]
+        public void TestChromaticNoteNormalNoteCreation(
+            ElementaryChromaticNotes elementaryChromaticNote,
+            int octaveOrder,
+            NotationSymbols modifier,
+            int staffPosition)
+        {
+            ChromaticNote chromaticNote = new ChromaticNote(elementaryChromaticNote, octaveOrder, modifier, staffPosition);
+
+            Assert.IsNotNull(chromaticNote);
+            Assert.AreEqual(elementaryChromaticNote, chromaticNote.ElementaryNote);
+            Assert.AreEqual(octaveOrder, chromaticNote.OctaveOrder);
+            Assert.AreEqual(modifier, chromaticNote.NoteModifierSymbol);
+            Assert.AreEqual(staffPosition, chromaticNote.StaffPositionIndex);
+        }
+
+        [Theory]
+        [TestCase(ElementaryChromaticNotes.E, -1, NotationSymbols.None, 1)]
+        [TestCase(ElementaryChromaticNotes.E, 1, NotationSymbols.WholeNote, 1)]
+        [TestCase(ElementaryChromaticNotes.E, 1, NotationSymbols.None, -1)]
+        public void TestChromaticNoteInvalidNoteCreation(
+            ElementaryChromaticNotes elementaryChromaticNote,
+            int octaveOrder,
+            NotationSymbols modifier,
+            int staffPosition)
+        {
+            TestDelegate invalidConstruction = () => new ChromaticNote(elementaryChromaticNote, octaveOrder, modifier, staffPosition);
+            Assert.Throws<ArgumentException>(invalidConstruction);
+        }
+    }
+}
