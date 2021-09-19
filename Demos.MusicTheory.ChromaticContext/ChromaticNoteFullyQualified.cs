bae9f3ceb7c8ec2b7bf0d7991347805b5dd9dfbb
Demo30 <wiesnertoma@gmail.com>
2021-09-19 12:27:41 +0200

    cleanup

--- Demos.MusicTheory.ChromaticContext/ChromaticNoteFullyQualified.cs
+++ Demos.MusicTheory.ChromaticContext/ChromaticNoteFullyQualified.cs
@@ -1,9 +1,10 @@
 ﻿using Demos.MusicTheory.Abstractions.ChromaticContext;
 using Demos.MusicTheory.Abstractions.Commons;
 using Demos.MusicTheory.Commons;
-using Demos.MusicTheory.Contexts.ChromaticContext;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using static Demos.MusicTheory.ChromaticContext.Constants.ChromaticContextConstants;
 
 namespace Demos.MusicTheory.ChromaticContext
 {
@@ -16,7 +17,16 @@ namespace Demos.MusicTheory.ChromaticContext
         public int Order { get; init; }
         public int ChromaticContextIndex => GetChromaticIndex();
 
+        private int QualityChromaticOffset => (int)Quality;
         private readonly ChromaticNoteElementary baseChromaticCharacteristic;
+        private static readonly Dictionary<NotationSymbols, int> _modifierChromaticCorrection = new()
+        {
+            { NotationSymbols.None, 0 },
+            { NotationSymbols.Sharp, 1 },
+            { NotationSymbols.Flat, -1 },
+            { NotationSymbols.DoubleSharp, +2 },
+            { NotationSymbols.DoubleFlat, -2 }
+        };
 
         public ChromaticNoteFullyQualified(ChromaticNoteQuality qualifier, int order, NotationSymbols modifier)
         {
@@ -40,59 +50,36 @@ namespace Demos.MusicTheory.ChromaticContext
 
         private string GetModifierString(NotationSymbols modifier)
         {
-            return Modifier == NotationSymbols.None ? "" : Modifier.ToString();
+            return modifier == NotationSymbols.None ? "" : modifier.ToString();
         }
 
         private int GetChromaticIndex()
         {
-            Func<NotationSymbols, int> getModifierNumber = (modifier) =>
-            {
-                switch (modifier)
-                {
-                    case NotationSymbols.None: return 0;
-                    case NotationSymbols.Sharp: return 1;
-                    case NotationSymbols.Flat: return -1;
-                    case NotationSymbols.DoubleSharp: return +2;
-                    case NotationSymbols.DoubleFlat: return -2;
-                    default: return 0;
-                }
-            };
+            Func<NotationSymbols, int> getChromaticOffsetModification = (modifier)
+                => _modifierChromaticCorrection.ContainsKey(modifier) ? _modifierChromaticCorrection[modifier] : 0;
 
-            const int elementaryStep = 1;
-            const int standardStep = elementaryStep * 2;
+            int chromaticOffset =
+                (Order * CHROMATIC_STEPS_FULL_OCTAVE) +
+                GetBaseChromaticOffset() +
+                getChromaticOffsetModification(Modifier);
 
-            int diatonicCorrection =
-                (Order * -elementaryStep) + 
-                ((int)Quality > (int)ChromaticNoteQuality.E ? -elementaryStep : 0);
-
-            // max index in octave minus one step correction due to diatonic scale
-            int stepsInFullOctave =
-                standardStep * (int)ChromaticNoteQuality.B - elementaryStep;
-
-            int baseNumberWithoutModifier =
-                stepsInFullOctave * Order +
-                (standardStep * (int)Quality) +
-                diatonicCorrection;
-
-            return baseNumberWithoutModifier + getModifierNumber(Modifier);
+            return chromaticOffset;
         }
+
+        private int GetBaseChromaticOffset() =>
+            ((2 * CHROMATIC_STEPS_ELEMENTARY_STEP) * QualityChromaticOffset) -
+            ((QualityChromaticOffset > (int)ChromaticNoteQuality.E) ? CHROMATIC_STEPS_ELEMENTARY_STEP : 0);
 
         private void CheckConstructionArgumentValidity(ChromaticNoteQuality quality, int octaveOrder, NotationSymbols modifier)
         {
             if (octaveOrder < 0)
-            {
                 throw new ArgumentException("Octave order cannot be negative.");
-            }
 
             if (quality == default)
-            {
                 throw new ArgumentException("Chromatic note quality needs to be specified.");
-            }
 
             if (!ChromaticNoteElementary.RelevantNotationSymbols.Contains(modifier))
-            {
                 throw new ArgumentException("Notation symbol is none of accepted types.");
-            }
         }
     }
 }
---
+++ Demos.MusicTheory.ChromaticContext/Constants/ChromaticContextConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Demos.MusicTheory.ChromaticContext.Constants
+{
+    public static class ChromaticContextConstants
+    {
+        public const int CHROMATIC_STEPS_ELEMENTARY_STEP = 1;
+        public const int CHROMATIC_STEPS_FULL_OCTAVE = 12;
+    }
+}
